f2bbf27ebb63c3eaac5c5812f029063014248838
Marwenproj <marwenkanoun94@gmail.com>
2023-07-29 08:29:13 +0100

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProduitsService } from './services/produits.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my_project';
+export class AppComponent implements OnInit{
+  title = 'Calcul de taxes';
+  list_produits:any=[];
+  Input_produits:any=[];
+  Output_produits:any=[];
+  historique_Output_produits:any=[];
+  id:Number=0
+  
+
+  constructor(private produits:ProduitsService) { }
+
+  ngOnInit() {
+    this.list_produits=this.produits.produits;
+  }
+
+  getItem(index:any){
+    let item=
+            {
+              ind:null,
+              indexP:null,
+              nb:0
+            }
+    let trouve=false
+    let i=0
+    while(i<this.Input_produits.length && trouve == false) {
+      if(this.Input_produits[i].indexP.id == this.list_produits[index].id){
+         this.Input_produits[i].nb+=1
+         trouve =true
+         break     
+      }
+      i++
+    }
+    if(!trouve){
+      item=
+      {
+        ind:index,
+        indexP:this.list_produits[index],
+        nb:1
+      }
+      this.Input_produits.push(item)
+    }   
+    this.facture()
+  }
+  facture(){
+     let tab_prod_taxe:any=[]
+     let taxe_totale=0
+     let t_prod=null
+     let montant_totale=0
+
+     let Prod_tae=
+                {
+                  ind:null,
+                  produit_Pht:null,
+                  produit_t:0,
+                  nb:null
+                }
+
+      for(let i=0;i<this.Input_produits.length;i++){
+        taxe_totale +=Math.round(this.produits.calcul_TTC(this.Input_produits[i].ind) * this.Input_produits[i].nb)
+        t_prod = (this.produits.calcul_TTC(this.Input_produits[i].ind) +this.Input_produits[i].indexP.prix) * this.Input_produits[i].nb
+        montant_totale +=(this.produits.calcul_TTC(this.Input_produits[i].ind) +this.Input_produits[i].indexP.prix)* this.Input_produits[i].nb
+        Prod_tae=
+                {
+                  ind:this.Input_produits[i].ind,
+                  produit_Pht:this.Input_produits[i],
+                  produit_t:t_prod,
+                  nb:this.Input_produits[i].nb
+                }
+                tab_prod_taxe.push(Prod_tae)
+      }
+      this.Output_produits=
+                {
+                  produit:tab_prod_taxe,
+                  Montant_des_taxes:taxe_totale,
+                  total :montant_totale
+                }
+      //this.Output_produits.push(facture_output)
+  }
+  
+
 }
---
+++ src/app/services/produits.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitsService {
+
+  produits = [
+    {
+      id: 1,
+      nom: "livre",
+      prix: 12.49,
+      importé: false,
+      marque: 1,
+      taxe:10,
+      t_importé:0
+    },
+    {
+      id: 2,
+      nom: "CD musical",
+      prix: 14.99,
+      importé: false,
+      marque: 1,
+      taxe:20,
+      t_importé:0
+    },
+    {
+      id: 3,
+      nom: "barre de chocolat",
+      prix: 0.85,
+      importé: false,
+      marque: 1,
+      taxe:0,
+      t_importé:0
+    },
+    {
+      id: 4,
+      nom: "boîte de chocolats importée",
+      prix: 10,
+      importé: true,
+      marque: 1,
+      taxe:0,
+      t_importé:5
+    },
+    {
+      id: 5,
+      nom: "flacon de parfum importé",
+      prix: 47.50,
+      importé: true,
+      marque: 1,
+      taxe:20,
+      t_importé:5
+    },
+    {
+      id: 6,
+      nom: "flacon de parfum importé",
+      prix: 27.99,
+      importé: true,
+      marque: 2,
+      taxe:20,
+      t_importé:5
+    },
+    {
+      id: 7,
+      nom: "flacon de parfum ",
+      prix: 18.99,
+      importé: false,
+      marque: 1,
+      taxe:20,
+      t_importé:0
+    },
+    {
+      id: 8,
+      nom: "boîtes de pilules contre la migraine ",
+      prix: 9.75,
+      importé: false,
+      marque: 1,
+      taxe:0,
+      t_importé:0
+    },
+    {
+      id: 9,
+      nom: "boîte de chocolats importé ",
+      prix: 11.25,
+      importé: true,
+      marque: 2,
+      taxe:0,
+      t_importé:5
+    }
+  ]
+
+  calcul_TTC(index:any){
+    let produit = this.produits[index]
+    let pttc = 0
+      if(!(produit.taxe == 0) && !(produit.t_importé == 0) ){
+        pttc = ((produit.prix * produit.taxe)/100) + ((produit.prix * produit.t_importé)/100)
+      }else if(produit.taxe == 0 && !(produit.t_importé == 0) ){
+        pttc = ((produit.prix * produit.t_importé)/100)
+      }else if(!(produit.taxe == 0) && produit.t_importé == 0 ){
+        pttc = ((produit.prix * produit.taxe)/100) 
+      }else{
+        pttc = 0
+      }
+      return pttc
+  }
+
+
+
+}
